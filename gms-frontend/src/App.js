c8b728557c43b743e87e4ffbf579480aa52152cd
Ankit Pandey <ankit221601@gmail.com>
2025-07-08 18:25:16 +0530

    tailwind-css installed

--- gms-frontend/src/App.js
+++ gms-frontend/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-red-800 border-slate-800 border-4">
+      Welcom to Gym Managment System(gms)
     </div>
   );
 }
